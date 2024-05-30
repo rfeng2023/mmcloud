0ff9989f90d9fb73b57dad8781ce8e818353fb35
Gao Wang <wangow@gmail.com>
2024-05-29 23:16:36 -0400

    Allow for specifying UI with environment variable VMUI

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,54 @@
 #!/bin/bash
 
-# Check if JupyterLab is available
-if command -v jupyter-lab &> /dev/null
-then
-    echo "JupyterLab is available. Starting JupyterLab ..."
-    jupyter-lab 
-else
-    echo "JupyterLab is not available. Starting tmate ..."
-    tmate -F
+# Function to check if a command is available
+is_available() {
+  command -v "$1" &> /dev/null
+}
+
+# Function to start the terminal server
+start_terminal_server() {
+  echo "Starting terminal server ..."
+  tmate -F
+}
+
+# Check if VMUI variable is set
+if [[ -z "${VMUI}" ]]; then
+  echo "No UI specified."
+  start_terminal_server
+  exit 0
 fi
+
+# Check the value of VMUI and start the corresponding UI
+case "${VMUI}" in
+  jupyter)
+    if is_available jupyter-lab; then
+      echo "JupyterLab is available. Starting JupyterLab ..."
+      jupyter-lab
+    else
+      echo "JupyterLab is not available."
+      start_terminal_server
+    fi
+    ;;
+  rstudio)
+    if is_available rstudio; then
+      echo "RStudio is available. Starting RStudio ..."
+      rstudio
+    else
+      echo "RStudio is not available."
+      start_terminal_server
+    fi
+    ;;
+  nvim)
+    if is_available nvim; then
+      echo "Nvim is available. Starting Nvim ..."
+      nvim
+    else
+      echo "Nvim is not available."
+      start_terminal_server
+    fi
+    ;;
+  *)
+    echo "Unknown UI specified: ${VMUI}."
+    start_terminal_server
+    ;;
+esac
