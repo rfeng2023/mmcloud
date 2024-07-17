a042786c1a277fc15771da5c4edb5ccf76851622
Ashley-Tung <atung@hmc.edu>
2024-07-17 09:15:37 -0700

    Fixed typo in float_submit line

--- src/mm_interactive.sh
+++ src/mm_interactive.sh
@@ -126,8 +126,8 @@ echo "Logging in to $OP_IP"
 float login -a "$OP_IP" -u "$user" -p "$password"
 
 # Adjust float submit command to include job name if provided
-float_submit="float submit -a $OP_IP -i $image -c $core -m $mem --vmPolicy $vm_policy_command --imageVolSize $image_vol_size --gateway g-9xahbrb5rkbs0ic8yz
-if [[ -n "$job_name" ]]; thenylk --migratePolicy [cpu.disable=true,mem.disable=true,stepAuto=true,evadeOOM=true] --publish $publish --securityGroup $securityGroup $dataVolumeOption --vmPolicy [onDemand=true] --withRoot --allowList [m*]"
+float_submit="float submit -a $OP_IP -i $image -c $core -m $mem --vmPolicy $vm_policy_command --imageVolSize $image_vol_size --gateway g-9xahbrb5rkbs0ic8yzylk --migratePolicy [cpu.disable=true,mem.disable=true,stepAuto=true,evadeOOM=true] --publish $publish --securityGroup $securityGroup $dataVolumeOption --vmPolicy [onDemand=true] --withRoot --allowList [m*]"
+if [[ -n "$job_name" ]]; then 
     float_submit+=" -n $job_name"
 fi
 
